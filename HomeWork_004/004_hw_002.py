e4dd425f29d817430b319ace518a73d66ce9aec8
DobrijZmej <DobrijZmej@gmail.com>
2017-09-07 15:00:06 +0300

    Написать функцию, которая выводит двухмерный список в виде таблицы

--- HomeWork_004/004_hw_002.py
+++ HomeWork_004/004_hw_002.py
@@ -2,7 +2,7 @@ def print_list(in_lists):
     col_sizes = []
     for one_list in in_lists:
         for i, val in enumerate(one_list):
-            if(len(col_sizes) <= i):
+            if len(col_sizes) <= i:
                 col_sizes.append(0)
             col_sizes[i] = max(col_sizes[i], len(str(val)))
     #print(col_sizes)
