6541aefd0384e493725e41c6f542e0d649900359
DobrijZmej <DobrijZmej@gmail.com>
2017-09-07 14:46:56 +0300

    0
    Написать аналоги функций min и max

---
+++ HomeWork_004/004_hw_000.py
@@ -0,0 +1,39 @@
+def my_max(*in_list):
+    current_max = None
+    for i in in_list:
+        if current_max is None:
+            current_max = i
+        if i > current_max:
+            current_max = i
+    #print(in_list)
+    #print(current_max)
+    return current_max
+
+def my_max_sort(*in_list):
+    list = [i for i in in_list]
+    list.sort()
+    return list[-1]
+
+def my_min(*in_list):
+    current_max = None
+    for i in in_list:
+        if current_max is None:
+            current_max = i
+        if i < current_max:
+            current_max = i
+    #print(in_list)
+    #print(current_max)
+    return current_max
+
+def my_min_sort(*in_list):
+    list = [i for i in in_list]
+    list.sort(reverse=True)
+    return list[-1]
+
+numbers = (4,6,7,8, 54654, 654654, 54, 6546, -121)
+
+print(my_max(*numbers))
+print(my_max_sort(*numbers))
+
+print(my_min(*numbers))
+print(my_min_sort(*numbers))
